d70db4ea559b96af01307927f037512f0506b343
ptwitchapon <poonpim0203@gmail.com>
2019-07-26 09:56:37 +0630

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'frW/Q9nJfSFDCcE5s890KGwRAIWXqUwdTLi3Ie9Ljs6n3n8CyiHHN7v3RlsLOaPtcBjrixG54xs1Zu2ScPazMn1Iu9fnB+aaRAbFDHH930/ylQQ6/BRMPMcxT4sxAVDlKIEV/V1Q72T414rurOV+CgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
